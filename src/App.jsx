9f68156de75c935cb0dc9ee849b9a56eeafd0220
Thomas Allen <71492408+teasmade@users.noreply.github.com>
2021-04-14 15:06:39 +0200

    delete commented code for sample component as per pull request review

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import Map from './components/Map';
-// import SampleComponent from './components/SampleComponent';
 
 function App() {
   return (
     <div className="App">
-      {/* <SampleComponent /> */}
       <Map />
     </div>
   );
