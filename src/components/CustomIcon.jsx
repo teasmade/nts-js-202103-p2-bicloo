4078a6ba68fc40a839ab0d98ade6a23ba7528d9d
Thomas Allen <71492408+teasmade@users.noreply.github.com>
2021-04-14 09:28:59 +0200

    update comments in customicon component

--- src/components/CustomIcon.jsx
+++ src/components/CustomIcon.jsx
@@ -14,17 +14,12 @@ const CustomIcon = new Icon({
 
 export default CustomIcon;
 
-// import L from 'leaflet';
-
-// const CustomIcon = L.icon({
+// const CustomIcon = new Icon({
 //   iconUrl: '/leaf-green.png',
 //   shadowUrl: '/leaf-shadow.png',
-
 //   iconSize: [38, 95], // size of the icon
 //   shadowSize: [50, 64], // size of the shadow
 //   iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
 //   shadowAnchor: [4, 62], // the same for the shadow
 //   popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
 // });
-
-// export default CustomIcon;
