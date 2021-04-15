a40f920e6671d5fdb9ddf3dc5d1862dedf07075c
AurelBouchet <aurelien-bouchet@hotmail.fr>
2021-04-15 18:01:08 +0200

    feature-shop creation

---
+++ src/components/shop/Reward.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './reward.css';
+
+export default function Reward() {
+  return <div className="reward">rewardzz</div>;
+}
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Reward from './Reward';
+import './shop.css';
 
-const Shop = () => {
-  return <div>Shop</div>;
-};
-
-export default Shop;
+export default function Shop() {
+  return (
+    <div className="shopBody">
+      <div className="container">
+        <div className="xpbar">1468 XP disponibles</div>
+        <div className="choose">Choisissez votre récompense!</div>
+      </div>
+      <div className="rewards">
+        <Reward />
+        <Reward />
+        <Reward />
+        <Reward />
+        <Reward />
+        <Reward />
+      </div>
+    </div>
+  );
+}
