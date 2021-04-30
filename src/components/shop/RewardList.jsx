ab89371f4fe0b93a4fee519b023a807d2876121d
Thomas Allen <71492408+teasmade@users.noreply.github.com>
2021-04-30 10:21:04 +0200

    remove console logs and delete hard data import from reward list

--- src/components/shop/RewardList.jsx
+++ src/components/shop/RewardList.jsx
@@ -1,18 +1,14 @@
 /* eslint-disable react/prop-types */
 import Reward from './Reward';
-// import rewardsData from './rewardsData';
 import './reward.css';
 
 const RewardList = ({
-  user,
   rewardsToDisplay,
   onRewardClick,
   setClickedRewardKey,
 }) => {
   return (
     <div className="rewards">
-      {console.log(user)}
-      {console.log(rewardsToDisplay)}
       {rewardsToDisplay.map((reward, i) => {
         return (
           <Reward
