377aabf29c8d808aba16a8cf24974639993b0ab9
Antoine <antoinepoppe@hotmail.fr>
2021-05-07 13:56:14 +0200

    anim

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Menu from './components/menu/Menu';
 import UserChoice from './components/menu/UserChoice';
 import Profile from './components/profile/Profile';
@@ -12,49 +12,64 @@ import SignUp from './components/profile/signup/SignUp';
 import Map from './components/Map/Map';
 import AuthContext from './firebase/AuthContext';
 import ForgotPassword from './components/profile/ForgotPassword';
+import FirstLog from './components/Firstlog/FirstLog';
 
 function App() {
   const [isUserChoiceExpended, setIsUserChoiceExpended] = useState(false);
+  const [firstLogUser, setFirstLogUser] = useState(true);
+
+  useEffect(() => {
+    if (firstLogUser) {
+      setTimeout(() => {
+        setFirstLogUser(false);
+        localStorage.setItem('alreadyLogged', true);
+      }, 4000);
+    }
+  });
 
   return (
     <div className="App">
-      <AuthContext>
-        <Router>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/map">
-              <Map />
-            </Route>
-            <Route exact path="/profile">
-              <Profile />
-            </Route>
-            <Route exact path="/signIn">
-              <SignIn />
-            </Route>
-            <Route exact path="/signUp">
-              <SignUp />
-            </Route>
-            <Route exact path="/forgot-password">
-              <ForgotPassword />
-            </Route>
-            <Route exact path="/shop">
-              <Shop />
-            </Route>
-          </Switch>
-          <UserChoice
-            isUserChoiceExpended={isUserChoiceExpended}
-            closeUserChoicePopup={() => setIsUserChoiceExpended(false)}
-          />
-          <Menu
-            isUserChoiceExpended={isUserChoiceExpended}
-            clickOnUser={() => {
-              setIsUserChoiceExpended(!isUserChoiceExpended);
-            }}
-          />
-        </Router>
-      </AuthContext>
+      {firstLogUser && !localStorage.getItem('alreadyLogged') ? (
+        <FirstLog />
+      ) : (
+        <AuthContext>
+          <Router>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/map">
+                <Map />
+              </Route>
+              <Route exact path="/profile">
+                <Profile />
+              </Route>
+              <Route exact path="/signIn">
+                <SignIn />
+              </Route>
+              <Route exact path="/signUp">
+                <SignUp />
+              </Route>
+              <Route exact path="/forgot-password">
+                <ForgotPassword />
+              </Route>
+              <Route exact path="/shop">
+                <Shop />
+              </Route>
+            </Switch>
+            <UserChoice
+              isUserChoiceExpended={isUserChoiceExpended}
+              closeUserChoicePopup={() => setIsUserChoiceExpended(false)}
+            />
+            <Menu
+              isUserChoiceExpended={isUserChoiceExpended}
+              clickOnUser={() => {
+                setIsUserChoiceExpended(!isUserChoiceExpended);
+              }}
+            />
+          </Router>
+        </AuthContext>
+      )}
     </div>
   );
 }
