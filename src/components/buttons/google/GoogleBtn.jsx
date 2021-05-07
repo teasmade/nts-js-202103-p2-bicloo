3a81ade655d7ff60ca43f9038c12cfe66cff12a0
Crocoya <yassine.tababi@outlook.com>
2021-05-07 10:29:03 +0200

    GoogleBtn add

--- src/components/buttons/google/GoogleBtn.jsx
+++ src/components/buttons/google/GoogleBtn.jsx
@@ -1,34 +1,38 @@
-/* eslint-disable no-console */
 import React from 'react';
-import { GoogleLogin } from 'react-google-login';
+import { useHistory } from 'react-router-dom';
+import { GoogleProvider } from '../../../firebase/Firebase';
+import socialMediaAuth from '../../../firebase/service/Auth';
 import GoogleIcon from '../../../assets/img/google.svg';
-
-const responseGoogle = (response) => {
-  console.log(response);
-};
+import UserService from '../../../Services/UserService';
 
 export default function GoogleBtn() {
+  const history = useHistory();
+
+  const handleClick = async (provider) => {
+    const res = await socialMediaAuth(provider);
+    console.log('google result', res);
+    const nameFromEmail = res.email.includes('@')
+      ? res.email.split('@')[0]
+      : res.email;
+    UserService.createUserInDatabase(res.uid, nameFromEmail)
+      .then(() => {
+        history.push('/');
+      })
+      .catch((e) => console.error('catch error', e));
+  };
+
   return (
     <>
-      <GoogleLogin
-        render={(renderProps) => (
-          <button
-            onClick={renderProps.onClick}
-            disabled={renderProps.disabled}
-            className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs relative"
-            type="button"
-            style={{ transition: 'all .15s ease' }}
-          >
-            <img alt="..." className="w-5 mr-1" src={GoogleIcon} />
-            Google
-          </button>
-        )}
-        clientId="191681210422-cm71o1dtidple9ib9u0nqfb2m6sc5u23.apps.googleusercontent.com"
-        buttonText="GOOGLE"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy="single_host_origin"
-      />
+      <button
+        onClick={() => handleClick(GoogleProvider)}
+        type="button"
+        className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs relative"
+        style={{ transition: 'all .15s ease' }}
+      >
+        {' '}
+        <img alt="..." className="w-5 mr-1" src={GoogleIcon} />
+        Google
+      </button>
     </>
   );
 }
