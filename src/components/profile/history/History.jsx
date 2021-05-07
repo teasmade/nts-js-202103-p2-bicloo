32f525b1fcb119101e480d4092d87c22caf78d32
Antoine <antoinepoppe@hotmail.fr>
2021-05-07 10:41:14 +0200

    add journeys

--- src/components/profile/history/History.jsx
+++ src/components/profile/history/History.jsx
@@ -1,11 +1,10 @@
 import './History.css';
 
 // eslint-disable-next-line react/prop-types
-const History = ({ date, length, xp }) => {
+const History = ({ date, xp }) => {
   return (
     <div className="history-line">
       <span className="history-date">{date}</span>
-      <span className="history-length">{length} km</span>
       <span className="history-xp">{xp}XP</span>
     </div>
   );
